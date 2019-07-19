eb109ed77fb79565c9ccf41a447967a0bcadbb90
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:49:41 -0700

    Create ProgrammingIntro.java

---
+++ Projects/Thor/ProgrammingIntro.java
@@ -0,0 +1,18 @@
+public class ProgrammingIntro {
+
+public static void main(String[] args){
+    System.out.println("hi hi yeet duck clock dog yeet duck/n you are a duck");
+
+int myVarible =  17;
+boolean thisIsTrue = false;
+System.out.println(myVarible);
+System.out.println(thisIsTrue);
+}
+
+
+
+
+
+
+
+}
