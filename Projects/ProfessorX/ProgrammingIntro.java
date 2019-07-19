d80918239129d253c23551b9e955fecf811c3a42
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:02:24 -0700

    Create ProgrammingIntro.java

---
+++ Projects/ProfessorX/ProgrammingIntro.java
@@ -0,0 +1,17 @@
+public class ProgrammingIntro {
+
+    public static void main(String[] args){
+        System.out.println("Hello Beautiful World of Minecraft and Fortnite!");
+    
+    int Silas = 7;
+    boolean thisIsTrue = false;
+    String thisIsAString = "Pie Cody!!!!"
+System.out.println(Silas);
+System.out.println(thisIsTrue);
+System.out.println(thisIsAString);
+
+
+
+  
+
+}
