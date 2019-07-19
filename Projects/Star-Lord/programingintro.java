ef5b56100bffc877279d2d6744f9ae0af67765d5
EUReKACamper15 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:31:43 -0700

    Create programingintro.java

---
+++ Projects/Star-Lord/programingintro.java
@@ -0,0 +1,19 @@
+public class programingintro {
+
+public static void main (String[] args){
+    System.out.println("pie read!!!");
+int horse = 9;
+
+System.out.println(horse);
+
+boolean thisistrue = false;
+system.out.println(thisistrue);
+
+
+
+
+
+
+
+}
+}
