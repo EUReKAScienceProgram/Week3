98647aef22017a53d9d99922d66869a2539beab5
EUReKACamper7 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:16:31 -0700

    Create programmingintro.java

---
+++ Projects/ScarletWitch/programmingintro.java
@@ -0,0 +1,13 @@
+public class programmingintro {
+
+    public static void main (String[] args){
+        System.out.println("Tc eats");  }
+
+            int myVariable = 17;
+
+System.out.println("myVarialble");
+
+
+
+
+}
