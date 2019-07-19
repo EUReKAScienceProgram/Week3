8504d286d7db57e898604e8b90712256b2f0f902
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 15:59:18 -0700

    Create ProgrammingIntro.java

---
+++ Projects/WonderWoman/ProgrammingIntro.java
@@ -0,0 +1,27 @@
+public class ProgrammingIntro {
+
+    public static void main(String[] args){
+        System.out.println("Hello World of fornite and minecraft\n  ouga booga gemmi dam toes  ;;;\n reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\neeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\neeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\neeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+
+    int Ethan = 44411; 
+    boolean mingo = false;
+    String thirissi ="pieeeeeeee sndjnfiunwq";
+
+    System.out.println(Ethan);
+    System.out.println(mingo);
+    System.out.println(thirissi);
+   
+   
+   
+   
+   
+   
+    }
+
+
+
+
+
+
+
+}
