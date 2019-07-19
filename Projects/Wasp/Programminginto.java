0af0c8e594da6b226fab5e8415811c4423b0c071
EUReKACamper14 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:29:50 -0700

    Create Programminginto.java

---
+++ Projects/Wasp/Programminginto.java
@@ -0,0 +1,44 @@
+public class Programminginto {
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("");
+        int reece = 100;
+        boolean thisistrue = false;
+        System.out.println(reece);
+        System.out.println(thisistrue);
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
