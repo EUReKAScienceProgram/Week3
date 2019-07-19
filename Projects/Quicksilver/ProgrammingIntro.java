9affb5b453d46def22f157b36748022bc5f2e30b
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:24:48 -0700

    Create ProgrammingIntro.java

---
+++ Projects/Quicksilver/ProgrammingIntro.java
@@ -0,0 +1,22 @@
+public class ProgrammingIntro {
+
+    public static void main (String[] args){
+        System.out.println("Tech camp week three 2019 EUReKA!!");
+    
+        int Kate = 37;
+    
+        System.out.println ("Kate");
+    
+        boolean flamingo = true;
+        System.out.println(flamingo);
+        
+    
+    }
+     
+        
+
+    
+    
+    
+    
+} 
