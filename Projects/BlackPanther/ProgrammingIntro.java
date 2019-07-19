a03abe8452c16e2d4089c0a28f28f58d6c5f9f9e
EUReKACamper19 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:38:38 -0700

    Create ProgrammingIntro.java

---
+++ Projects/BlackPanther/ProgrammingIntro.java
@@ -0,0 +1,26 @@
+public class ProgrammingIntro {
+
+public static void main(String[] args){
+    System.out.println("Patooni!");
+
+int Liam = 7; 
+boolean thisisFalse = true;
+String thisisAString = "Pie Someone!";
+System.out.println(Liam);
+System.out.println(thisisFalse);
+System.out.println(thisisAString);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
