b01f0fadbe69235128228727a210794f2fa5d3a3
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:26:29 -0700

    Create programmingIntro.java

---
+++ Projects/Hulk/programmingIntro.java
@@ -0,0 +1,28 @@
+public class programmingIntro {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int result = myFunction(18, 74);
+        System.out.println(result);
+
+
+        // int thisIsAVariable = 3;
+        // double decimalNumber = 3.14;
+        // boolean trueOrFalse = true;
+        // char aLetter = 'M';
+        // String someText = "My dog is cute!";
+
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueOrFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+    }
+
+    public static int myFunction(int numberOne, int numberTwo){
+        return numberOne * numberTwo;
+               
+
+    }   
+}
