6b5d788a116220c26e6cd74794639912d6549fdd
EUReKACamper11 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-19 16:13:29 -0700

    Create ProgrammingIntro.java

---
+++ Projects/Ant-Man/ProgrammingIntro.java
@@ -0,0 +1,32 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class ProgrammingIntro {
+
+    public static void main(String[] args){
+        System.out.println("boi who is u\nokay whats up\nnanu nanu\nboi boi boi\nboi boi:)");
+    
+
+    int doggobox = 324;
+
+    System.out.println(doggobox);
+    
+
+    boolean True = false;
+    System.out.println(True);
+    
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
